fd96763093c8aff9cb1df81615996ff47bd41b1d
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-07-20 15:21:38 +0200

    HTML two date difference

--- Random number Generator/app.js
+++ Random number Generator/app.js
@@ -6,10 +6,10 @@ const generateBnt = document.querySelector(".generate");
 generateBnt.addEventListener("click", run);
 
 function run() {
-   let min_number = document.querySelector(".min").value;
-   let max_number = document.querySelector(".max").value;
+   let minNumber = document.querySelector(".min").value;
+   let maxNumber = document.querySelector(".max").value;
 
-   const number = randomInteger(min_number, max_number);
+   const number = randomInteger(minNumber, maxNumber);
 
    document.querySelector(".number-generated").innerHTML = number;
 }
