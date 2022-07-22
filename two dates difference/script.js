1f29ec5307813971680c864c3e3c8a47391af661
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-07-22 17:29:52 +0200

    finishing script.js, adding github link

--- two dates difference/script.js
+++ two dates difference/script.js
@@ -5,6 +5,19 @@ const output = document.querySelector("#output");
 const submit = document.querySelector(".submit-btn");
 
 submit.addEventListener("click", (e) => {
-   let start = new Date(starDate.value);
+   let start = new Date(startDate.value);
    let end = new Date(endDate.value);
+
+   let diff = (end - start) / (1000 * 3600 * 24);
+
+   // Creating output element
+   if (diff == 1) {
+      output.innerText = "Result: " + diff + " day";
+      container.append(output);
+      document.body.append(container);
+   } else {
+      output.innerText = "Result: " + diff + " days";
+      container.append(output);
+      document.body.append(container);
+   }
 });
