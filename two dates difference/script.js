fc9933dd2c27f8c1cb563509cc44ba455799f885
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-07-22 17:17:17 +0200

    starting javascript

---
+++ two dates difference/script.js
@@ -0,0 +1,10 @@
+const startDate = document.querySelector("#startDate");
+const endDate = document.querySelector("#endDate");
+const container = document.querySelector(".container");
+const output = document.querySelector("#output");
+const submit = document.querySelector(".submit-btn");
+
+submit.addEventListener("click", (e) => {
+   let start = new Date(starDate.value);
+   let end = new Date(endDate.value);
+});
