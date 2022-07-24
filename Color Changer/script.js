1b13aeb408ca421dd1ba3650500ff516c64da967
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-07-24 19:21:40 +0200

    changes Color Changer Project

--- Color Changer/script.js
+++ Color Changer/script.js
@@ -1,6 +1,7 @@
 const bg = document.querySelector(".bg-color");
 const output = document.querySelector(".output");
 
+//Creating event listener on click on the button
 const btn = document.querySelector(".btn").addEventListener("click", (e) => {
    let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
    bg.style.background = randomColor;
