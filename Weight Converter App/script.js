e6c4677c1d0bce696a3bd4eafdd43183dcbb1549
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-07-30 17:18:22 +0200

    javascript almost done + changes in HTML and CSS

--- Weight Converter App/script.js
+++ Weight Converter App/script.js
@@ -2,9 +2,25 @@ const kgRef = document.getElementById("kg");
 const lbsRef = document.getElementById("lbs");
 const ozRef = document.getElementById("oz");
 
-const converterBtn = document.querySelector(".converter-btn");
+const convertFromKg = () => {
+   let kg = kgRef.value;
 
-converterBtn.addEventListener("click", function () {
-   let from = document.getElementById("from");
-   let to = document.getElementById("to");
-});
+   lbsRef.value = (kg * 2.205).toFixed(2);
+   ozRef.value = (kg * 35.274).toFixed(2);
+};
+
+const convertFromLbs = () => {
+   let lbs = lbsRef.value;
+
+   kgRef.value = (kg / 2.205).toFixed(2);
+   ozRef.value = (kg * 16).toFixed(2);
+};
+
+const convertFromOz = () => {
+   let oz = kgRef.value;
+
+   kgRef.value = (kg / 35.274).toFixed(2);
+   ozRef.value = (kg / 16).toFixed(2);
+};
+
+// https://codepen.io/Sicontis/pen/RZwpmx -- Prova questo
