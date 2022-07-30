b28ff82e7fc8afe0d4787fdca6962cf3c245c04c
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-07-30 18:27:54 +0200

    Finished Weight converter App javascript

--- Weight Converter App/script.js
+++ Weight Converter App/script.js
@@ -1,26 +1,33 @@
-const kgRef = document.getElementById("kg");
-const lbsRef = document.getElementById("lbs");
-const ozRef = document.getElementById("oz");
+let kgRef = document.getElementById("kg");
+let lbsRef = document.getElementById("lbs");
+let ozRef = document.getElementById("oz");
+let inputs = document.querySelectorAll("input");
 
-const convertFromKg = () => {
+const resetBtn = document.getElementById("reset-btn");
+
+let convertFromKg = () => {
    let kg = kgRef.value;
-
    lbsRef.value = (kg * 2.205).toFixed(2);
    ozRef.value = (kg * 35.274).toFixed(2);
 };
 
-const convertFromLbs = () => {
+let convertFromLbs = () => {
    let lbs = lbsRef.value;
-
-   kgRef.value = (kg / 2.205).toFixed(2);
-   ozRef.value = (kg * 16).toFixed(2);
+   kgRef.value = (lbs / 2.205).toFixed(2);
+   ozRef.value = (lbs * 16).toFixed(2);
 };
 
-const convertFromOz = () => {
-   let oz = kgRef.value;
-
-   kgRef.value = (kg / 35.274).toFixed(2);
-   ozRef.value = (kg / 16).toFixed(2);
+let convertFromOz = () => {
+   let oz = ozRef.value;
+   kgRef.value = (oz / 35.274).toFixed(2);
+   lbsRef.value = (oz / 16).toFixed(2);
 };
 
-// https://codepen.io/Sicontis/pen/RZwpmx -- Prova questo
+resetBtn.addEventListener("click", () => {
+   inputs.forEach((input) => (input.value = ""));
+});
+
+kg.addEventListener("input", convertFromKg);
+lbs.addEventListener("input", convertFromLbs);
+oz.addEventListener("input", convertFromOz);
+window.addEventListener("load", convertFromKg);
