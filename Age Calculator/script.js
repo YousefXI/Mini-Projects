133b4bb9ccf0e6403dadffba58bbc4d3b5298796
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-08-01 22:37:17 +0200

    finished javascript + changes in html and css

--- Age Calculator/script.js
+++ Age Calculator/script.js
@@ -1,6 +1,6 @@
 // Variables For Datepicker
 const datePicker = document.getElementById("datePicker");
-const choseDate = document.getElementById("datePicker");
+const chosenDate = document.getElementById("chosenDate");
 
 // Variables For Printing Value
 
@@ -11,11 +11,43 @@ const ageMinutes = document.getElementById("ageMinutes");
 const ageSeconds = document.getElementById("ageSeconds");
 
 datePicker.addEventListener("change", function () {
-   const options = { year: "number", month: "long", day: "numeric" };
-   const selectedDate = new Date(this.value);
-   const DOB = selectedDate.toLocaleDateString("en-US", options); //ERROR?
+   var options = { year: "number", month: "long", day: "numeric" };
+   var selectedDate = new Date(this.value);
+   var DOB = selectedDate.toLocaleDateString(); //Grabbing The Date Selected
 
-   console.log("DOB IS: " + DOB);
+   console.log("DOB IS : " + DOB);
+   chosenDate.innerHTML = "DOB : " + " " + DOB;
+
+   var millisecondsBtwDOB = Date.parse(DOB);
+   var millisecondsBtwNow = Date.now();
+
+   var ageInMilliseconds = millisecondsBtwNow - millisecondsBtwDOB;
+
+   //console.log(ageInMilliseconds);
+
+   var miliSeconds = ageInMilliseconds;
+   var second = 1000;
+   var minute = second * 60;
+   var hour = minute * 60;
+   var day = hour * 24;
+   var month = day * 30;
+   var year = day * 365;
+
+   //Starting Calculation
+
+   var years = Math.round(miliSeconds / year);
+   var months = years * 12;
+   var days = years * 365;
+   var hours = Math.round(miliSeconds / hour);
+   var seconds = Math.round(miliSeconds / second);
+
+   //Printing Values in Box
+
+   ageYears.innerHTML = years;
+   ageMonths.innerHTML = months;
+   ageDays.innerHTML = days;
+   ageMinutes.innerHTML = hours;
+   ageSeconds.innerHTML = seconds;
 });
 
 // https://www.youtube.com/watch?v=_jd6x6Ze9Qg&list=PLimvEekCsmZ99z-kjg6fZB0ZzXGfIVSYk
