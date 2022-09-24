426e5c0aacfbc21afb9abb8a37c9ea73aa3ef455
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-09-24 23:08:44 +0200

    update new Date

--- Countdown Timer/app.js
+++ Countdown Timer/app.js
@@ -5,7 +5,7 @@ const seconds = document.getElementById("seconds");
 
 const currentYear = new Date().getFullYear();
 
-const fallSeasonTime = new Date(`September 22 2022 00:00:00`);
+const fallSeasonTime = new Date(`September 22 2023 00:00:00`);
 
 //Update Countdown time
 function updateCountowntime() {
